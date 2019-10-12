cb35bc683507bb31df758a0eb6e2c555d6296714
marieahmad <marie8hiday@gmail.com>
2019-10-12 18:12:49 +0700

    belajar js

---
+++ javascript/index.js
@@ -0,0 +1,66 @@
+// dasar
+console.log('hello world');
+
+// logika pemrograman
+const a = 'mobil balap'
+console.log(a);
+
+// String
+const pembeli = 'beli'
+const penjual = 'jual barang'
+const data = `${pembeli} dan ${penjual}`
+console.log(data);
+
+// number
+const angka = 1
+console.log(angka);
+
+const data1 = 10 + 10 / 10
+const data2 = 11 * 2 - 2
+console.log(data1,data2);
+
+//  Boolean
+let benar = true
+
+// Array
+const dataArray = ['andi', 'putri', 'sabrina', 1, true, undefined, null]
+
+dataArray [0]
+dataArray [1]
+dataArray [3]
+dataArray [4]
+dataArray.length
+
+console.log(dataArray[3]);
+
+// testing boolean
+let number = 10
+let result1 = (number == 10)
+let result2 = (number == 7)
+let result3 = (number == '10')
+console.log(result3);
+
+let result4 = (number === '10')
+let result5 = (number === 10)
+console.log(result4);
+
+// 
+let time = new Date()
+console.log(time);
+
+let time2 = new Date('October 12, 2019 18:03:12')
+console.log(time2);
+
+let getYears = time.getFullYear()
+console.log(getYears);
+
+const buah = ['apple', 'orange', 'lemon', 'pepaya']
+console.log(buah);
+
+
+
+
+
+
+
+
